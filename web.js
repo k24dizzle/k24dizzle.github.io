84aab0d07dfad42fe15cd7d5eab8c588196b619d
k24dizzle <zhaok24@uw.edu>
2016-06-13 15:03:12 -0700

    fixed it?

--- web.js
+++ web.js
@@ -8,21 +8,21 @@ $(document).ready(function() {
 
         $('html, body').animate({
             scrollTop: $(dest).offset().top
-        }, 933);
+        }, 1933);
     });
 });
 
-var velocity = 0.33;
+var velocity = 0.2;
 
 function update(){ 
     var pos = $(window).scrollTop(); 
     $('.morestuff').each(function() { 
         var $element = $(this);
         // subtract some from the height b/c of the padding
-        var height = $element.height()-18;
+        var height = $element.height();
         console.log("height" + height);
         console.log(pos);
-        var newPos = -1 *  (height - Math.round((pos - height) * velocity));
+        var newPos = -1 *  (height/5 - Math.round((pos - height) * velocity));
         var data = '50% ' + newPos + 'px';
         console.log(newPos);
         if (newPos <= 0) {
